18ed32eb37e9b5fba4071d4dd77d91307540553b
capstere <jesper.fredriksson2@gmail.com>
2025-05-14 21:20:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,12 +2,12 @@
 
 const CACHE_NAME = 'varkamp-cache-v1';
 const URLS_TO_CACHE = [
-  '/',                   // Roten
+  '/',                   
   '/index.html',
   '/css/styles.css',
   '/js/script.js',
   '/manifest.json',
-  '/assets/data/puzzles.json',         // Gåtor + statiska sidor
+  '/assets/data/puzzles.json',
   '/assets/icons/play.svg',
   '/assets/icons/spring.svg',
   '/assets/icons/fight.svg',
@@ -20,10 +20,9 @@ const URLS_TO_CACHE = [
   '/assets/images/stego.png',
   '/assets/images/arcimboldo-spring-thumb.jpg',
   '/assets/images/arcimboldo-spring.jpg',
-  '/assets/images/icon-512.png'        // Startsides-ikon
+  '/assets/icons/icon-512.png'  // rätt sökväg till stora startsides-ikonen
 ];
 
-// När service worker installeras: cacha allt
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -32,7 +31,6 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// När ny version aktiveras: rensa gamla caches
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
@@ -44,7 +42,6 @@ self.addEventListener('activate', event => {
   );
 });
 
-// På fetch: försök från cache, annars nätet, annars offline-meddelande
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
